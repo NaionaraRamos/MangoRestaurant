20bbacb7be6c79fdd3d88f4db316874332a043bd
Naionara Maceda <nramosmaceda@outlook.com>
2021-11-02 23:33:57 -0300

    Sessão 14 - API de Pedidos

---
+++ Mango.Services.OrderAPI/Messaging/AzureServiceBusConsumer.cs
@@ -0,0 +1,97 @@
+﻿using Azure.Messaging.ServiceBus;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Mango.Services.OrderAPI.Messages;
+using Mango.Services.OrderAPI.Repositories;
+using Mango.Services.OrderAPI.Models;
+using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+
+namespace Mango.Services.OrderAPI.Messaging
+{
+    public class AzureServiceBusConsumer : IAzureServiceBusConsumer
+    {
+        private readonly string serviceBusConnectionString;
+        private readonly string subscriptionName;
+        private readonly string checkoutMessageTopic;
+        private readonly OrderRepository _orderRepository;
+        private readonly IConfiguration _configuration;
+        private ServiceBusProcessor checkoutProcessor;
+
+        public AzureServiceBusConsumer(OrderRepository orderRepository, IConfiguration configuration)
+        {
+            _orderRepository = orderRepository;
+            _configuration = configuration;
+
+            serviceBusConnectionString = _configuration.GetValue<string>("ServiceBusConnectString");
+            subscriptionName = _configuration.GetValue<string>("SubscriptionName");
+            checkoutMessageTopic = _configuration.GetValue<string>("CheckoutMessageTopic");
+
+            var client = new ServiceBusClient(serviceBusConnectionString);
+            checkoutProcessor = client.CreateProcessor(checkoutMessageTopic, subscriptionName);
+        }
+
+        public async Task Start()
+        {
+            checkoutProcessor.ProcessMessageAsync += OnCheckoutMessageReceived;
+            checkoutProcessor.ProcessErrorAsync += ErrorHandler;
+            await checkoutProcessor.StartProcessingAsync();
+        }
+
+        public async Task Stop()
+        {
+            await checkoutProcessor.StartProcessingAsync();
+            await checkoutProcessor.DisposeAsync();
+        }
+
+        Task ErrorHandler(ProcessErrorEventArgs args)
+        {
+            Console.WriteLine(args.Exception.ToString());
+            return Task.CompletedTask;
+        }
+
+        private async Task OnCheckoutMessageReceived(ProcessMessageEventArgs args)
+        {
+            var message = args.Message;
+            var body = Encoding.UTF8.GetString(message.Body);
+
+            CheckoutHeaderDto checkoutHeaderDto = JsonConvert.DeserializeObject<CheckoutHeaderDto>(body);
+
+            OrderHeader orderHeader = new()
+            {
+                UserId = checkoutHeaderDto.UserId,
+                FirstName = checkoutHeaderDto.FirstName,
+                LastName = checkoutHeaderDto.LastName,
+                OrderDetails = new List<OrderDetails>(),
+                CardNumber = checkoutHeaderDto.CardNumber,
+                CouponCode = checkoutHeaderDto.CouponCode,
+                CVV = checkoutHeaderDto.CVV,
+                DiscountTotal = checkoutHeaderDto.DiscountTotal,
+                Email = checkoutHeaderDto.Email,
+                ExpiryMonthYear = checkoutHeaderDto.ExpiryMonthYear,
+                OrderTime = DateTime.Now,
+                OrderTotal = checkoutHeaderDto.OrderTotal,
+                PaymentStatus = false,
+                Phone = checkoutHeaderDto.Phone,
+                PickupDateTime = checkoutHeaderDto.PickupDateTime
+            };
+
+            foreach(var detailsList in checkoutHeaderDto.CartDetails)
+            {
+                OrderDetails orderDetails = new()
+                {
+                    ProductId = detailsList.ProductId,
+                    ProductName = detailsList.Product.Name,
+                    Price = detailsList.Product.Price,
+                    Count = detailsList.Count
+                };
+                orderHeader.CartTotalItems += detailsList.Count;
+                orderHeader.OrderDetails.Add(orderDetails);
+            }
+
+            await _orderRepository.AddOrder(orderHeader);
+        }
+    }
+}
